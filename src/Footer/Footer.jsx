e380759ead24245f82b562617d43833d938229ca
sajibt <zerosajibint@gmail.com>
2022-12-26 21:47:31 +0600

    final

---
+++ src/Footer/Footer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Footer.scss";
+
+const Footer = () => {
+  return (
+    <section>
+      <div className="footer">
+        <h1 className="about">About</h1>
+        <p className="us">
+          Ezyto is a easy note taking application.You can easily create, update
+          and delete note easily . Built with Expresjs Backend and React
+          Frontend.{" "}
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Footer;
